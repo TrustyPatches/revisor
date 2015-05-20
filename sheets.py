966ad75773080a82273d33177de50a238570b41d
TrustyPatches <fpendlebury@gmail.com>
2015-05-20 22:19:11 +0100

    Complete basic functionality, rendering revisor implementation obsolete

--- sheets.py
+++ sheets.py
@@ -1,10 +1,47 @@
 import re
+import sys
 import glob
+
+if sys.version_info[0] >= 3:
+    get_input = input
+else:
+    get_input = raw_input
 
 def main():
     print("It's all okay")
+    printTitle()
     sl = SheetLibrary()
+   
+    print("Please select a sheet number:\n")
+    
+    for i in range(len(sl.get_sheet_titles())):
+        print("     " + str(i + 1) + ". " + sl.get_sheet_titles()[i])
 
+    selected_sheet = sl.sheets[int(get_input()) - 1]
+     
+    for i in range(len(selected_sheet.get_topics_titles())):
+        print("     " + str(i + 1) + ". " + selected_sheet.get_topics_titles()[i])
+
+    selected_topic = selected_sheet.topics[int(get_input()) - 1]
+
+    for qa in selected_topic.qa_pairs:
+        print("".join(qa.question))
+        get_input("...") 
+
+    get_input("That's all the questions. Ready for the answers now..?\n")
+
+    for qa in selected_topic.qa_pairs:
+        print("".join(qa.answer))
+        get_input()
+
+    print("")
+
+def printTitle():
+    print("")
+    print("         /=================\\")
+    print("         |   S H E E T S   | ")
+    print("         \\=================/")
+    print("")
 
 class SheetLibrary:
 
@@ -41,10 +78,16 @@ class SheetLibrary:
             sheets.append(sheet)
         return sheets
 
+    def get_sheet_titles(self):
+        sheet_titles = []
+        for s in self.sheets:
+            sheet_titles.append(s.title)
+        return sheet_titles
+
     def grab_title(self, line):
         title_match_pattern = r'(^#\s+)([\w+\s*]*)'
         title_match = re.match(title_match_pattern, line)
-        return title_match.group(2).lower().rstrip('\n') if (title_match) else ""
+        return title_match.group(2).rstrip('\n') if (title_match) else ""
     
     def set_section(self, section, line):
         section_match_pattern = r'(^##\s+)([\w+\s*]*)'
@@ -70,17 +113,29 @@ class Sheet:
         for i in range(topics_count):
             new_topic = Topic(topics_questions[i], topics_answers[i])
             topics.append(new_topic)
+        
         return topics
 
-    def create_topics_raw(self, section):
+
+    def get_topics_titles(self):
+        topic_titles = []
+        for t in self.topics:
+            topic_titles.append(t.title)
+        return topic_titles
+
+    def create_topics_raw(self, section):       
         topic_match_pattern = r'(^###\s+)([\w+\s*]*)'
+        section_match_pattern = r'(^##\s+)([\w+\s*]*)'
         topic = []
         topics_section = []
         for line in section:
             if (topic and re.match(topic_match_pattern, line)):
                 topics_section.append(topic)
                 topic = []
-            topic.append(line)
+            if (not re.match(section_match_pattern, line) and
+                not re.match(r'(\s*\n+\s*)', line)):
+                topic.append(line)
+        print(topics_section)
         return topics_section
 
 class Topic:
@@ -93,13 +148,10 @@ class Topic:
         for line in questions:
             title_match = re.match(topic_match_pattern, line)
             if (title_match):
-                return title_match.group(2).lower().rstrip('\n').rstrip(' ')
+                return title_match.group(2).rstrip('\n').rstrip(' ')
         return "Untitled"
 
     def create_qa_pairs(self, questions, answers):
-        # Divide questions and answers into arrays of strings containing 
-        # each question/answer
-        # Iterate through both, creating QA_Pairs and then spitting them out 
         qa_questions = self.divide_qa(questions)
         qa_answers = self.divide_qa(answers)
         if (len(qa_questions) != len(qa_answers)):
@@ -122,8 +174,8 @@ class Topic:
         for line in group[1:]:
             if (qa and re.match(qa_match_pattern, line)):
                 qa_group.append(qa)
-                qa = []
-            if (not re.match(r'(\s*\n\s*)', line)):
+                qa = []     
+            if (not re.match(r'(\s*\n+\s*)', line)):
                 qa.append(line)
         return qa_group
 
